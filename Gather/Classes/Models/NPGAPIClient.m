bbdc73a30c4aa67a89b19bc2f1d4c4f2fb15822d
Tony DiPasquale <tony@thoughtbot.com>
2014-04-10 14:16:10 -0400

    Refactor api class

--- Gather/Classes/Models/NPGAPIClient.m
+++ Gather/Classes/Models/NPGAPIClient.m
@@ -15,34 +15,31 @@
 
 static NSString *const NPGAPIBaseURL = @"https://novproject-gather.herokuapp.com/api/v1/";
 
+typedef void (^NPGAPIResponseCompletionHandler)(id json, NSHTTPURLResponse *response, NSError *error);
+
 @implementation NPGAPIClient
+
+#pragma mark - Public Methods
 
 + (void)createUserWithName:(NSString *)name completionHandler:(NPGUserCompletionHandler)handler
 {
     NSDictionary *params = @{@"name": name,
                              @"deviceID": [[[UIDevice currentDevice] identifierForVendor] UUIDString]};
 
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"user" relativeToURL:[NSURL URLWithString:NPGAPIBaseURL]]];
-    request.HTTPBody = [NSJSONSerialization dataWithJSONObject:params options:0 error:nil];
-    request.HTTPMethod = @"POST";
-    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-
-    [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-
-        if (httpResponse.statusCode != 200) {
-            NSLog(@"Error: %@", error);
-            NSLog(@"Returned: %@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
-            handler(nil);
+    [self postWithPath:@"user" dictionary:params completionHandler:^(id json, NSHTTPURLResponse *response, NSError *error) {
+        if (response.statusCode != 200) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                handler(nil);
+            });
             return;
         }
 
-        NPGUser *user = [NPGUserFactory userWithJSON:[NSJSONSerialization JSONObjectWithData:data options:0 error:nil]];
+        NPGUser *user = [NPGUserFactory userWithJSON:json];
 
         dispatch_async(dispatch_get_main_queue(), ^{
             handler(user);
         });
-    }] resume];
+    }];
 }
 
 + (void)registerUserWithDeviceToken:(NSString *)deviceToken completionHandler:(void (^)(void))handler
@@ -52,50 +49,33 @@ static NSString *const NPGAPIBaseURL = @"https://novproject-gather.herokuapp.com
     }
 
     NSDictionary *params = @{@"deviceToken": deviceToken};
+    NSString *path = [NSString stringWithFormat:@"user/%@/register", [NPGAppSession sharedAppSession].currentUser.objectID];
 
-    NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"user/%@/register", [NPGAppSession sharedAppSession].currentUser.objectID] relativeToURL:[NSURL URLWithString:NPGAPIBaseURL]];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    request.HTTPBody = [NSJSONSerialization dataWithJSONObject:params options:0 error:nil];
-    request.HTTPMethod = @"POST";
-    [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-
-    [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-
-        if (httpResponse.statusCode != 200) {
-            NSLog(@"Error: %@", error);
-            NSLog(@"Returned: %@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
-        }
-
+    [self postWithPath:path dictionary:params completionHandler:^(id json, NSHTTPURLResponse *response, NSError *error) {
         dispatch_async(dispatch_get_main_queue(), ^{
             handler();
         });
-    }] resume];
+    }];
 }
 
 + (void)fetchGroupsWithCoordinate:(CLLocationCoordinate2D)coordinate range:(double)range completionHandler:(NPGGroupsCompletionHandler)handler
 {
-    NSString *urlString = [NSString stringWithFormat:@"groups?latitude=%f&longitude=%f&range=%f", coordinate.latitude, coordinate.longitude, range];
-    NSURL *url = [NSURL URLWithString:urlString relativeToURL:[NSURL URLWithString:NPGAPIBaseURL]];
+    NSString *path = [NSString stringWithFormat:@"groups?latitude=%f&longitude=%f&range=%f", coordinate.latitude, coordinate.longitude, range];
 
-    [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
-
-        if (httpResponse.statusCode != 200) {
-            NSLog(@"Returned: %@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
-
+    [self getWithPath:path completionHandler:^(id json, NSHTTPURLResponse *response, NSError *error) {
+        if (response.statusCode != 200) {
             dispatch_async(dispatch_get_main_queue(), ^{
                 handler(@[]);
             });
             return;
         }
 
-        NSArray *groups = [NPGGroupFactory groupsWithJSON:[NSJSONSerialization JSONObjectWithData:data options:0 error:nil]];
+        NSArray *groups = [NPGGroupFactory groupsWithJSON:json];
 
         dispatch_async(dispatch_get_main_queue(), ^{
             handler(groups);
         });
-    }] resume];
+    }];
 }
 
 + (void)createGroupWithType:(NSString *)type time:(NSDate *)time coordinate:(CLLocationCoordinate2D)coordinate completionHandler:(NPGGroupCompletionHandler)handler
@@ -106,26 +86,61 @@ static NSString *const NPGAPIBaseURL = @"https://novproject-gather.herokuapp.com
                              @"longitude": @(coordinate.longitude),
                              @"owner": [NPGAppSession sharedAppSession].currentUser.objectID};
 
-    NSURL *url = [NSURL URLWithString:@"groups" relativeToURL:[NSURL URLWithString:NPGAPIBaseURL]];
+    [self postWithPath:@"groups" dictionary:params completionHandler:^(id json, NSHTTPURLResponse *response, NSError *error) {
+        if (response.statusCode != 200) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                handler(nil);
+            });
+            return;
+        }
+
+        NPGGroup *group = [NPGGroupFactory groupWithJSON:json];
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            handler(group);
+        });
+    }];
+}
+
+#pragma mark - Private Methods
+
++ (void)postWithPath:(NSString *)path dictionary:(NSDictionary *)dictionary completionHandler:(NPGAPIResponseCompletionHandler)handler
+{
+    NSURL *url = [NSURL URLWithString:path relativeToURL:[NSURL URLWithString:NPGAPIBaseURL]];
     NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    request.HTTPBody = [NSJSONSerialization dataWithJSONObject:params options:0 error:nil];
+    request.HTTPBody = [NSJSONSerialization dataWithJSONObject:dictionary options:0 error:nil];
     request.HTTPMethod = @"POST";
     [request addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
 
     [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
         NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
 
-        if (httpResponse.statusCode != 200) {
+        NSError *err;
+        id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&err];
+
+        if (err) {
             NSLog(@"Returned: %@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
-            handler(nil);
-            return;
         }
 
-        NPGGroup *group = [NPGGroupFactory groupWithJSON:[NSJSONSerialization JSONObjectWithData:data options:0 error:nil]];
+        handler(json, httpResponse, error);
+    }] resume];
+}
 
-        dispatch_async(dispatch_get_main_queue(), ^{
-            handler(group);
-        });
++ (void)getWithPath:(NSString *)path completionHandler:(NPGAPIResponseCompletionHandler)handler
+{
+    NSURL *url = [NSURL URLWithString:path relativeToURL:[NSURL URLWithString:NPGAPIBaseURL]];
+
+    [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+
+        NSError *err;
+        id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&err];
+
+        if (err) {
+            NSLog(@"Returned: %@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+        }
+
+        handler(json, httpResponse, error);
     }] resume];
 }
 
